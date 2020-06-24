3d6d1f399ef00aeccb9c8fdc961009c93e1c41bf
gcyoung1 <47953565+gcyoung1@users.noreply.github.com>
2020-06-24 14:49:29 -0700

    Finished RestaurantPage

--- RestaurantPage/src/historyPage.js
+++ RestaurantPage/src/historyPage.js
@@ -1,9 +1,7 @@
 export function loadPage(){
     let container = document.querySelector('#content');
-    let prev = container.querySelector('div');
-    container.removeChild(prev);
     let div=document.createElement('div');    
-    let header = document.createElement('h1');
+    let header = document.createElement('h2');
     let image = document.createElement('img');
     image.src = "https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg";
     header.textContent="Our Storied History";
--- RestaurantPage/src/index.js
+++ RestaurantPage/src/index.js
@@ -1,25 +1,31 @@
-import home from './homePage.js';
-import history from './historyPage.js';
+let home = require('./homePage.js');
+let history = require('./historyPage.js');
 
 function wipe(){
     let prev = container.querySelector('div');
     container.removeChild(prev);
 }
 
+
+let body=document.querySelector('body');
 let tabdiv = document.createElement('div');
 tabdiv.style.display = 'grid';
-tabdiv.style.gridTemplateColumns = '50px 50px';
+tabdiv.style.gridTemplateColumns = '150px 150px';
 let hometab = document.createElement('h1');
-hometab.addEventListener('click', ()=>{wipe(); home.loadPage()});
+hometab.addEventListener('click', ()=>{
+    wipe(); home.loadPage()});
 hometab.style.gridColumns = "1/2";
+hometab.textContent="Home";
 tabdiv.appendChild(hometab);
 
 let historytab = document.createElement('h1');
-historytab.addEventListener('click', ()=>{wipe(); history.loadPage()});
+historytab.addEventListener('click', ()=>{
+    wipe(); history.loadPage()});
 historytab.style.gridColumns = "2/3";
+historytab.textContent="History";
 tabdiv.appendChild(historytab);
 let container = document.querySelector('#content');
-container.appendChild(tabdiv);
+body.appendChild(tabdiv);
 
 
 
